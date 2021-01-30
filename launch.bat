d15aa855caea0d739e13f8d1412e32df0159b825
Sectral027 <65045136+Sectral027@users.noreply.github.com>
2021-01-30 12:00:37 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,6 +1,18 @@
 echo off
 
+color 0a
+
 
 echo Welcome to
 echo.
 echo MultiTerminal v0.0.1
+echo.
+echo.
+echo.
+echo.
+echo  ************************
+echo  *       Tools          *
+echo  ************************
+
+
+pause
