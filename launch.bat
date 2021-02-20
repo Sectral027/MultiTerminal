09a9fb74bd0046b3209f48ccf4c37cff907e782b
Sectral027 <65045136+Sectral027@users.noreply.github.com>
2021-02-20 22:49:06 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -5,7 +5,7 @@ color 0a
 
 echo Welcome to
 echo.
-echo MultiTerminal v0.0.1
+echo MultiTerminal v0.1.1
 echo.
 echo.
 echo.
@@ -13,6 +13,24 @@ echo.
 echo  ************************
 echo  *       Tools          *
 echo  ************************
+echo  * 1. Calculator        *
+echo  ************************
+echo  * 2. Github Page (Dev) *
+
+
+// La opción que se va a elegir (c) Xabia
+
+set /p opcion=Choose what to do=
+
+
+if "%opcion%"== "1" goto 1
+
+:1
+start calc.exe
+
+
+
+
 
 
 pause
