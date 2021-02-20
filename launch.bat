026b2be1bc968b6e4d920cacad748c6b1390ae36
Sectral027 <65045136+Sectral027@users.noreply.github.com>
2021-02-20 22:56:51 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,11 +1,16 @@
 echo off
 
+:inicio
+
+
+cls
 color 0a
+
 
 
 echo Welcome to
 echo.
-echo MultiTerminal v0.1.1
+echo MultiTerminal v0.1.5
 echo.
 echo.
 echo.
@@ -15,18 +20,43 @@ echo  *       Tools          *
 echo  ************************
 echo  * 1. Calculator        *
 echo  ************************
-echo  * 2. Github Page (Dev) *
-
-
-// La opción que se va a elegir (c) Xabia
-
+echo  * 2. Github Page       *
+echo  ************************
+echo  * 3. Credits           *
+echo  ************************
 set /p opcion=Choose what to do=
 
 
 if "%opcion%"== "1" goto 1
+if "%opcion%"== "2" goto 2
+if "%opcion%"== "3" goto 3
+
 
 :1
 start calc.exe
+
+goto inicio
+
+:2
+
+start 
+https://github.com/Sectral027
+
+goto inicio
+
+:3
+
+cls
+
+Echo.
+echo Software, idea and developement by Spectral_027. 
+echo.
+echo (c) Spectral_027
+echo. 
+echo (c) Xabia
+
+goto inicio
+
 
 
 
